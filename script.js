379209d4995b411b13fa0ab40b94a01ec7edb1db
Akhileshlad1171 <ladharsh059@gmail.com>
2026-05-05 18:39:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,16 @@
 function playSong(id) {
   let audio = document.getElementById(id);
 
+  // stop all others
   document.querySelectorAll("audio").forEach(a => {
     a.pause();
     a.currentTime = 0;
   });
 
-  audio.volume = 0;
   audio.play();
-
-  // fade in
-  let vol = 0;
-  let fade = setInterval(() => {
-    if (vol < 1) {
-      vol += 0.05;
-      audio.volume = vol;
-    } else {
-      clearInterval(fade);
-    }
-  }, 200);
-}
-let current = 0;
-let memories = document.querySelectorAll(".memory");
-
-function showMemory(index) {
-  memories.forEach(m => m.classList.remove("active"));
-  memories[index].classList.add("active");
 }
 
-function nextMemory() {
-  current = (current + 1) % memories.length;
-  showMemory(current);
-}
-
-function prevMemory() {
-  current = (current - 1 + memories.length) % memories.length;
-  showMemory(current);
-}
-
-function startStory() {
-  document.getElementById("intro").style.display = "none";
-  document.getElementById("main").style.display = "block";
-    memories = document.querySelectorAll(".memory"); // important
-  showMemory(0); // show first memory
-}
+// scroll animation
 window.addEventListener("scroll", () => {
   document.querySelectorAll(".memory").forEach(el => {
     let pos = el.getBoundingClientRect().top;
